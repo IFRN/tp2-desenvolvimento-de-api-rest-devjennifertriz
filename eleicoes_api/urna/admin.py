970850326ca8c3eda700e4e0a5b8baa631cd1f2d
Jennifer Beatriz Medeiros do Nascimento <jennifer.n@academico.ifrn.edu.br>
2026-05-13 14:41:36 -0300

    fix: setup do projeto e modelos iniciais

--- eleicoes_api/urna/admin.py
+++ eleicoes_api/urna/admin.py
@@ -1,4 +1,11 @@
 ﻿from django.contrib import admin
+from .models import Eleitor, Eleicao, Candidato, Voto, AptidaoEleitor, RegistroVotacao
 
 # Register your models here.
 
+admin.site.register(Eleitor)
+admin.site.register(Eleicao)
+admin.site.register(Candidato)
+admin.site.register(Voto)
+admin.site.register(AptidaoEleitor)
+admin.site.register(RegistroVotacao)
